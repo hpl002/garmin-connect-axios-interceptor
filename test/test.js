fcbf4f99c383ea450ad4ba0c6c9d5f7d7f421ba6
Herman Stoud Platou <hsplatou@gmail.com>
2022-04-01 21:37:40 +0200

    test: throw err if missing env var + test cookie formatter

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 const { getCookies } = require("../src")
+const { formatCookies } = require("../src/utils")
 jest.setTimeout(30000)
 /*
 
@@ -10,6 +11,43 @@ middleware: try to perform a new request after the cookie has been invalided (ha
 
 */
 
+test("format cookies", async () => {
+    const cookies = [
+        {
+            "name": "SESSIONID",
+            "value": "redacted",
+            "domain": "connect.garmin.com",
+            "path": "/",
+            "expires": -1,
+            "size": 57,
+            "httpOnly": true,
+            "secure": true,
+            "session": true,
+            "sameSite": "Lax",
+            "sameParty": false,
+            "sourceScheme": "Secure",
+            "sourcePort": 443
+        },
+        {
+            "name": "__cflb",
+            "value": "redacted",
+            "domain": "connect.garmin.com",
+            "path": "/",
+            "expires": 1648840887.257783,
+            "size": 51,
+            "httpOnly": true,
+            "secure": true,
+            "session": false,
+            "sameSite": "None",
+            "sameParty": false,
+            "sourceScheme": "Secure",
+            "sourcePort": 443
+        }
+    ]
+
+    expect(formatCookies(cookies).split(";").length == 2).toBe(true)
+});
+
 test("missing env var GARMIN_CONNECT_USERNAME", async () => {
     process.env.GARMIN_CONNECT_USERNAME = "test"
     process.env.GARMIN_CONNECT_PASSWORD = "test"
@@ -18,7 +56,7 @@ test("missing env var GARMIN_CONNECT_USERNAME", async () => {
         await getCookies(true)
         expect(false).toBe(true)
     } catch (error) {
-        expect(error.message).toBe("missng env var GARMIN_CONNECT_USERNAME")
+        expect(error.message).toBe("missing env var GARMIN_CONNECT_USERNAME")
     }
 });
 
@@ -30,60 +68,6 @@ test("missng env var GARMIN_CONNECT_PASSWORD", async () => {
         await getCookies(true)
         expect(false).toBe(true)
     } catch (error) {
-        expect(error.message).toBe("missng env var GARMIN_CONNECT_PASSWORD")
+        expect(error.message).toBe("missing env var GARMIN_CONNECT_PASSWORD")
     }
-});
-
-test("get cookies", async () => {
-    try {
-        //const { cookies } = await getCookies(true)
-        const cookies = [
-            {
-                "name": "SESSIONID",
-                "value": "redacted",
-                "domain": "connect.garmin.com",
-                "path": "/",
-                "expires": -1,
-                "size": 57,
-                "httpOnly": true,
-                "secure": true,
-                "session": true,
-                "sameSite": "Lax",
-                "sameParty": false,
-                "sourceScheme": "Secure",
-                "sourcePort": 443
-            },
-            {
-                "name": "__cflb",
-                "value": "redacted",
-                "domain": "connect.garmin.com",
-                "path": "/",
-                "expires": 1648840887.257783,
-                "size": 51,
-                "httpOnly": true,
-                "secure": true,
-                "session": false,
-                "sameSite": "None",
-                "sameParty": false,
-                "sourceScheme": "Secure",
-                "sourcePort": 443
-            }
-        ]
-
-        cookies.forEach(c => {
-            expect(["SESSIONID", "__cflb"].includes(c.name)).toBe(true)
-        });
-    } catch (error) {
-        throw error
-    }
-
-
-});
-
-
-
-
-test("", async () => {
-    const { cookies } = await getCookies(true)
-    console.log(cookies);
 });
